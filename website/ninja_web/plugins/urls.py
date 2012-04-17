99d4c4c65116ae77e4f73bf14dd979f4a42e08a1
Matías Herranz matiasherranz@gmail.com <matiasherranz@gmail.com>
2012-04-17 20:37:20 -0300

    Urls for plugin edit/update.

--- website/ninja_web/plugins/urls.py
+++ website/ninja_web/plugins/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 *-*
 from django.conf.urls.defaults import patterns, url
-from plugins.views import (plugin_submit, plugin, plugins)
+from plugins.views import (plugin_submit, get_plugin, plugins, plugin_edit)
 from common.views import (schemes, official, community)
 
 
@@ -11,7 +11,8 @@ urlpatterns = patterns('plugins.views',
     url(r'^community', community),
 
     # from plugins.views
+    url(r'^edit/(?P<plugin_id>\d+)/$', plugin_edit, name="plugin_edit"),
     url(r'^submit/$', plugin_submit, name="plugin_submit"),
-    url(r'^(?P<plugin_id>\d+)/$', plugin, name="plugin_detail"),
+    url(r'^(?P<plugin_id>\d+)/$', get_plugin, name="plugin_detail"),
     url(r'^$', plugins, name="plugins"),
 )

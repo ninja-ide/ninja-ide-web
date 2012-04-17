2ef50b2f8ffbc580a85c56d4c8360f004c36a4d0
Matías Herranz matiasherranz@gmail.com <matiasherranz@gmail.com>
2012-04-17 20:11:58 -0300

    More changes in plugins/urls.py

--- website/ninja_web/plugins/urls.py
+++ website/ninja_web/plugins/urls.py
@@ -5,9 +5,12 @@ from common.views import (schemes, official, community)
 
 
 urlpatterns = patterns('plugins.views',
+    # from common.views
     url(r'^schemes', schemes),
     url(r'^official', official),
     url(r'^community', community),
+
+    # from plugins.views
     url(r'^submit/$', plugin_submit, name="plugin_submit"),
     url(r'^(?P<plugin_id>\d+)/$', plugin, name="plugin_detail"),
     url(r'^$', plugins, name="plugins"),

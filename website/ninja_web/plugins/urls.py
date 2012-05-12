dc03bc4e3fb45204b96a69353e13de668d22cdc5
Matías Herranz matiasherranz@gmail.com <matiasherranz@gmail.com>
2012-05-12 13:39:33 -0300

    Added API urls.

--- website/ninja_web/plugins/urls.py
+++ website/ninja_web/plugins/urls.py
@@ -1,18 +1,22 @@
 # -*- coding: utf-8 *-*
 from django.conf.urls.defaults import patterns, url
+
+from plugins.api import (schemes, official_plugins, community_plugins)
 from plugins.views import (plugin_submit, get_plugin, plugins, plugin_edit)
-from common.views import (schemes, official, community)
 
 
 urlpatterns = patterns('plugins.views',
-    # from common.views
-    url(r'^schemes', schemes),
-    url(r'^official', official),
-    url(r'^community', community),
-
-    # from plugins.views
     url(r'^edit/(?P<plugin_id>\d+)/$', plugin_edit, name="plugin_edit"),
     url(r'^submit/$', plugin_submit, name="plugin_submit"),
     url(r'^(?P<plugin_id>\d+)/$', get_plugin, name="plugin_detail"),
     url(r'^$', plugins, name="plugins"),
+)
+
+urlpatterns += patterns('plugins.api',
+
+    # API
+    url(r'^community/', community_plugins),
+    url(r'^official/', official_plugins),
+
+    url(r'^schemes', schemes),
 )

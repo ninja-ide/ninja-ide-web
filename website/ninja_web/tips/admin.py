4beedc67443743e9254bf0698683aa507a1bead7
Pedro Mourelle <pedromagnus@gmail.com>
2012-06-13 20:56:41 -0300

    Pep8 fixes

---
+++ website/ninja_web/tips/admin.py
@@ -0,0 +1,8 @@
+# from django.contrib import admin
+
+# from tips.models import Tip
+
+# class TipAdmin(admin.ModelAdmin):
+#     fields = ('text', 'has_links', 'datetime_since', 'datetime_until', 'container_class')
+
+# admin.site.register(Tip, TipAdmin)
--- website/ninja_web/tips/models.py
+++ website/ninja_web/tips/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
-class Tip(models.Model):
-	""" Tips to show in the page.
-		`text` is the text to be shown with autoescape off, so
-		links are allowed.
-		`datetime_since` date since when will be available
-		`datetime_until` date since won't be available
-	"""
-	text = models.TextField()
-	has_links = models.BooleanField(default=False, help_text=u'Needed for escaping characters')
-	datetime_since = models.DateTimeField(auto_now_add=True, help_text=u'Since when is available for publishing')
-	datetime_until = models.DateTimeField(help_text=u'Date until it is viewable')
-	container_class = models.TextField(max_length=50, help_text=u'HTML class for the wrapper')
 
-	def __unicode__(self):
-		return u'%s...' % text[30:]
+class Tip(models.Model):
+    """ Tips to show in the page.
+        `text` is the text to be shown with autoescape off, so
+        links are allowed.
+        `datetime_since` date since when will be available
+        `datetime_until` date since won't be available
+    """
+    text = models.TextField()
+    has_links = models.BooleanField(default=False, help_text=u'Needed for escaping characters')
+    datetime_since = models.DateTimeField(auto_now_add=True, help_text=u'Since when is available for publishing')
+    datetime_until = models.DateTimeField(help_text=u'Date until it is viewable')
+    container_class = models.TextField(max_length=50, help_text=u'HTML class for the wrapper')
+
+    def __unicode__(self):
+        return u'%s...' % self.text[30:]

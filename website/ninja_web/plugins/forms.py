e3a7e3bc4a5fd4f7c9afeb951265d2a98746ebe6
Pedro Mourelle <pedromagnus@gmail.com>
2012-06-14 03:52:17 -0300

    Added class for required field

--- website/ninja_web/plugins/forms.py
+++ website/ninja_web/plugins/forms.py
@@ -9,3 +9,5 @@ class PluginForm(forms.ModelForm):
     class Meta:
         model = Plugin
         exclude = ['user', 'upload_date']
+        required_css_class = 'required'
+        error_css_class = 'error'

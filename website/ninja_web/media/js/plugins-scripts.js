1228e0ed223a8634207b3bf17fadada3f1fe06b1
Pedro Mourelle <pedromagnus@gmail.com>
2012-07-01 20:56:11 -0300

    Schemes preview style. Some fixes to scheme section. New NinjaIDE 2.0 intro style.

--- website/ninja_web/media/js/plugins-scripts.js
+++ website/ninja_web/media/js/plugins-scripts.js
@@ -21,83 +21,62 @@ function twoDecimals(amount) {
 }
 
 function rate_plugin(id, rate, url) {
-	/*  register the rate given to a plugin marked with id.
-	    it tries to register the rate at the given url.
-	*/
-	var post_url = url+"?plugin_id=" + id + "&rate=" + rate;
-	elem = jQuery("#plugin_"+id);
+    /*  register the rate given to a plugin marked with id.
+        it tries to register the rate at the given url.
+    */
+    var post_url = url+"?plugin_id=" + id + "&rate=" + rate;
+    var elem = $("#plugin_"+id.toString());
 
-	if (elem) {
-	  // the rate container
-	  var rate_container = elem.find(".rate-container");
+    if (!elem) {
+      alert("Error founding the plugin you want to vote. :S");
+    } else {
+      // the rate container
+      var rate_container = elem.find(".rate-container");
 
-	  jQuery.ajax({
-	    url: post_url,
-	    dataType: 'json',
-	    success: function(data) {
-	      // send the rate & update the rate of this plugin 
+      jQuery.ajax({
+        url: post_url,
+        dataType: 'json',
+        success: function(data) {
+          // send the rate & update the rate of this plugin
 
-	      //console.debug("data = " + data.ok.toString());
-	      var modal = jQuery("<div class='msg'>");
-	      modal.css('display', 'block');
-	      elem.prepend(modal);
+          var modal = $("<div class='msg'>");
+          modal.css('display', 'block');
+          elem.prepend(modal);
 
-	      var new_div = jQuery('<div>');
-	      new_div.addClass('message');
+          var new_div = $('<div>');
+          new_div.addClass('message');
 
-	      if (data.ok) {
-	        // hiding rate container
-	        rate_container
-	        .fadeOut();
+          if (data.ok) {
+            // hiding rate container
+            rate_container.fadeOut();
 
-	        // update plugin avg. rate
-	        rate_container
-	        .find('.plugin-rate')
-	        .html(twoDecimals(data.plugin_rate.toString()));
+            // update plugin avg. rate
+            rate_container
+              .find('.plugin-rate')
+              .html(twoDecimals(data.plugin_rate.toString()));
 
-	        // update plugin rates count
-	        rate_container
-	        .find('.plugin-rate-times')
-	        .html("( "+data.plugin_rate_times+" )");
+            // update plugin rates count
+            rate_container
+              .find('.plugin-rate-times')
+              .html("( "+data.plugin_rate_times+" )");
 
-	        // showing rate container
-	        rate_container
-	        .fadeIn();
+            // showing rate container
+            rate_container.fadeIn();
 
-	      } else {
-	        new_div.addClass('error');
-	      }
+          } else {
+            alert("error");
+            new_div.addClass('error');
+          }
 
-        // message to user
-	      var p = "<p>" + data.msg + "</p>";
-	      new_div.append(p);
+          // message to user
+          var p = "<p>" + data.msg + "</p>";
+          new_div.append(p);
 
-        // nice message shown up
-	      modal.append(new_div)
-	      .delay(4000)
-	      .slideUp(1000);
-	    }
-	  });
-	}
+          // nice message shown up
+          modal.append(new_div)
+            .delay(4000)
+            .slideUp(1000);
+        }
+      });
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-

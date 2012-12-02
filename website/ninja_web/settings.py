9a5fb57e3adb70e8790894c20010ea0c924ea7e3
Pedro Mourelle <pmourelle@machinalis.com>
2012-12-02 06:49:25 -0300

    Removed comments, added sorl.thumbnail and staticfiles from django1.4

--- website/ninja_web/settings.py
+++ website/ninja_web/settings.py
@@ -68,6 +68,7 @@ TEMPLATE_LOADERS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder'
 )
 
 MIDDLEWARE_CLASSES = (

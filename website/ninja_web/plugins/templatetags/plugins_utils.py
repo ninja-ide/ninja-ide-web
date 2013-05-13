248c9635d3a0215e0e232f150d360b8647b398cc
Pedro Mourelle <pedromagnus@gmail.com>
2013-05-13 10:30:58 -0300

    Templatetag for getting last plugins.

--- website/ninja_web/plugins/templatetags/plugins_utils.py
+++ website/ninja_web/plugins/templatetags/plugins_utils.py
@@ -1,83 +1,51 @@
-import math, re
+from datetime import datetime
 
-from django.template import Library, Node, TemplateSyntaxError, VariableDoesNotExist, resolve_variable
-from django.conf import settings
+from django.template import Library, Node, TemplateSyntaxError  # , resolve_variable
+
+from plugins.models import Plugin
 
 register = Library()
 
 
-class ShowPluginsNode(Node):
-	""" Default rounding is to the whole unit """
-	def __init__(self, stars, total_stars, round_to, handler=None, identifier=None):
-		self.stars = stars
-		self.total_stars = int(total_stars)
-		self.rounder = ROUNDERS[round_to.lower()]
-		self.handler = handler
-		self.identifier = identifier
+class LastPluginsNode(Node):
+    def __init__(self, max_plugins=5, var_name='last_plugins'):
+        self.max = int(max_plugins)
+        self.var_name = var_name
 
-	def merge_star(self, pos, fraction, identifier):
-		alt, src = STARS[fraction]
-		if self.handler:
-			pos += 1
-			return EX_IMG_TEMPLATE % (identifier, pos, identifier, self.handler, identifier, pos, FULL_FOLDER + src, alt)
-		else:
-			return IMG_TEMPLATE % (FULL_FOLDER + src, alt)
+    def render(self, context):
+        plugins = Plugin.objects.filter(upload_date__lte=datetime.now).order_by('-upload_date')[:self.max]
 
-	def render(self, context):
-		try:
-			stars = resolve_variable(self.stars, context)
-		except VariableDoesNotExist:
-			try:
-				stars = float(self.stars)
-			except:
-				return ""
-		try:
-			identifier = resolve_variable(self.identifier, context)
-		except VariableDoesNotExist:
-			identifier = self.identifier
+        if self.var_name:
+            context[self.var_name] = plugins
+        else:
+            return plugins
 
-		stars = round(stars * self.rounder) / self.rounder
-
-		fraction, integer = math.modf(stars)
-		output = []
-
-		if self.handler:
-			output.append(DIV_TEMPLATE % identifier)
-		for i in range(self.total_stars):
-			if i < integer:
-				output.append(self.merge_star(i, 1.0, identifier))
-			elif i == integer and fraction:
-				output.append(self.merge_star(i, fraction, identifier))
-			else:
-				output.append(self.merge_star(i, 0.0, identifier))
-		if self.handler:
-			output.append(END_DIV_TEMPLATE)
-
-		return "".join(output)
-
-class ShowStarsScriptNode(Node):
-	def render(self, context):
-		return JS_TEMPLATE
+        return ''
 
 
 def last_plugins(parser, token):
-	def syntax_error():
-		raise TemplateSyntaxError("example: show_stars <value> of <total> round to %s [on change call <handler> with <identifier>]" % "|".join(ROUNDERS))
-	args = token.contents.split()
-	if len(args) == 7:
-		match = CMD_PATTERN.match(token.contents)
-	elif len(args) == 13:
-		match = EX_CMD_PATTERN.match(token.contents)
-	else:
-		syntax_error()
-	if not match:
-		syntax_error()
+    """ Get the latest plugins (by upload date) and returns them
+        --------
+        usage:
 
-	return ShowStarsNode(*match.groups())   
+        {% last_plugins 4 %}
+        will create/load the context variable 'last_plugins' with
+        the last 4 plugins uploaded.
 
-
-def do_show_stars_script(parser, token):
-	return ShowStarsScriptNode()
+        {% last_plugins 4 as poopi %}
+        same as before but the variable is 'poopi'
+    """
+    def syntax_error():
+        raise TemplateSyntaxError("example: last_plugins <value> [as <context_variable>]")
+    args = token.contents.split()
+    if len(args) == 4:
+        if args[2] != 'as':
+            syntax_error()
+        ret = LastPluginsNode(args[1], args[3])
+    elif len(args) == 2:
+        ret = LastPluginsNode(args[1])
+    else:
+        syntax_error()
+    return ret
 
 register.tag("last_plugins", last_plugins)
-register.tag("plugins", plugins)

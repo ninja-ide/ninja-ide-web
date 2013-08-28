a9591a0447989886745e815a13445f13612372d6
Pedro Mourelle <pedromagnus@gmail.com>
2013-08-28 04:08:24 -0300

    Added simple test. Continued django's CBVs traslation.

--- website/ninja_web/plugins/urls.py
+++ website/ninja_web/plugins/urls.py
@@ -1,19 +1,32 @@
 # -*- coding: utf-8 *-*
-from django.conf.urls.defaults import patterns, url
+from django.conf.urls import patterns, url
 
 from plugins.api import get_plugins_dict
-from plugins.views import plugin_submit, get_plugin, plugin_edit
-from plugins.views import PluginListView
+from plugins.views import plugin_submit
+from plugins.views import (PluginDetailView,
+                           PluginListView,
+                           PluginEditView)
 
 
 # VIEWS
 urlpatterns = patterns(
     'plugins.views',
 
-    url(r'^edit/(?P<plugin_id>\d+)/$', plugin_edit, name="plugin_edit"),
-    url(r'^submit/$', plugin_submit, name="plugin_submit"),
-    url(r'^(?P<plugin_id>\d+)/$', get_plugin, name="plugin_detail"),
-    url(r'^$', PluginListView.as_view(), name="plugins"),
+    url(r'^edit/(?P<plugin_id>\d+)/$',
+        PluginEditView.as_view(),
+        name="plugin_edit"),
+
+    url(r'^submit/$',
+        plugin_submit,
+        name="plugin_submit"),
+
+    url(r'^(?P<plugin_id>\d+)/$',
+        PluginDetailView.as_view(),
+        name="plugin_detail"),
+
+    url(r'^$',
+        PluginListView.as_view(),
+        name="plugin_list"),
 )
 
 # API
@@ -21,11 +34,25 @@ urlpatterns += patterns(
     'plugins.api',
 
     # URLs for existing/old versions
-    url(r'^official/$', get_plugins_dict, {'query': 'official'}),
-    url(r'^community/$', get_plugins_dict, {'query': 'community'}),
+    url(r'^official/$',
+        get_plugins_dict,
+        {'query': 'official'}),
 
-    url(r'^api/official/$', get_plugins_dict, {'query': 'official'}),
-    url(r'^api/community/$', get_plugins_dict, {'query': 'community'}),
-    url(r'^api/(?P<query>\w+)/$', get_plugins_dict),
-    url(r'^api/$', get_plugins_dict),
+    url(r'^community/$',
+        get_plugins_dict,
+        {'query': 'community'}),
+
+    url(r'^api/official/$',
+        get_plugins_dict,
+        {'query': 'official'}),
+
+    url(r'^api/community/$',
+        get_plugins_dict,
+        {'query': 'community'}),
+
+    url(r'^api/(?P<query>\w+)/$',
+        get_plugins_dict),
+
+    url(r'^api/$',
+        get_plugins_dict),
 )
--- website/ninja_web/plugins/views.py
+++ website/ninja_web/plugins/views.py
@@ -1,10 +1,6 @@
 # encoding: utf-8
 from decimal import Decimal
 
-from tagging.models import Tag
-
-from django.views.generic import ListView
-
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
@@ -12,10 +8,13 @@ from django.db import IntegrityError
 from django.http import HttpResponse
 from django.shortcuts import redirect
 from django.utils import simplejson
+from django.views.generic import DetailView, ListView, UpdateView
+
+from tagging.models import Tag
 
 from common.utils import render_response
-from plugins.forms import PluginForm
-from plugins.models import Vote, Plugin
+from .forms import PluginForm
+from .models import Vote, Plugin
 
 try:
     import json
@@ -54,6 +53,10 @@ def filter_by_tag(request, tag_id):
     return render_response(request, 'plugins.html', context)
 
 
+class PluginCreateView(UpdateView):
+    pass
+
+
 @login_required
 def plugin_submit(request):
     context = {}
@@ -65,9 +68,10 @@ def plugin_submit(request):
             new_plugin = form.save(commit=False)
             new_plugin.user = request.user
             new_plugin.save()
-            messages.info(request, u'Plugin submitted correctly little dragon.')
+            messages.info(request,
+                          u'Plugin submitted correctly little dragon.')
 
-            return redirect('plugins')
+            return redirect('plugin_list')
         else:
             messages.error(
                 request,
@@ -131,61 +135,70 @@ def rate_plugin(request):
     return response
 
 
-def get_plugin(request, plugin_id=None):
+class PluginDetailView(DetailView):
     """ Returns the details/info about plugin with plugin_id given
         Redirects to plugins if plugin does not exist.
     """
-    context = {}
+    model = Plugin
+    context_object_name = 'plugin'
+    slug_field = 'pk'
+    slug_url_kwarg = 'plugin_id'
 
-    try:
-        context['plugin'] = Plugin.objects.get(pk=plugin_id)
-    except Plugin.DoesNotExist:
-        messages.info(request, u'The plugin you look for no longer exists dude. If you are a bot, please, GTFOOH.')
-        return redirect('plugins')
-
-    # some another extra info for this plugin:
-    # context['extra'] = blabla
-    return render_response(request, 'plugin-detail.html', context)
+    def get(self, request, **kwargs):
+        try:
+            self.model.objects.get(pk=kwargs['plugin_id'])
+            return super(PluginDetailView, self).get(request, **kwargs)
+        except self.model.DoesNotExist:
+            messages.info(request, u'The plugin you look for no longer exists '
+                                   'dude. If you are a bot, please, GTFOOH.')
+            return redirect(reverse('plugin_list'))
 
 
 class PluginListView(ListView):
-    pass
+    model = Plugin
 
 
-def plugins(request):
-    context = {}
+class PluginEditView(UpdateView):
+    model = Plugin
+    slug_name = 'plugin_id'
+    form_class = PluginForm
+    template_name = 'plugin-submit.html'
 
-    #if some-category-selected:
-    #    context['plugin-category'] = the-category
+    def post(self, request, *args, **kwargs):
+        return super(PluginEditView, self).post(request, *args, **kwargs)
 
-    plugins = Plugin.objects.all()
+    def form_valid(self, form):
+        plugin = form.save()
+        messages.info(self.request, u'Plugin {} updated correctly little '
+                                    'dragon!'.format(plugin))
+        success_url = reverse('user_detail', args=(self.request.user.username))
 
-    context['plugins'] = plugins
-    return render_response(request, 'plugins.html', context)
+    def form_not_valid(self, form):
+        messages.error(
+            self.request,
+            u'Something went wrong in your submit. Please, check it.')
 
 
-@login_required
-def plugin_edit(request, plugin_id):
-    """ Plugin edition/update view."""
-    context = {}
+# @login_required
+# def plugin_edit(request, plugin_id):
+#     """ Plugin edition/update view."""
+#     context = {}
 
-    plugin = Plugin.objects.get(id=plugin_id)
-    form = PluginForm(request.POST or None,
-                      request.FILES or None,
-                      instance=plugin)
+#     plugin = Plugin.objects.get(id=plugin_id)
+#     form = PluginForm(request.POST or None,
+#                       request.FILES or None,
+#                       instance=plugin)
 
-    if request.method == 'POST':
-        if form.is_valid():
-            plugin = form.save()
-            #plugin.save()
-            messages.info(request, u'Plugin updated correctly little dragon!')
+#     if request.method == 'POST':
+#         if form.is_valid():
+#             plugin = form.save()
 
-            redirect_url = reverse('user_detail', args=(request.user.username,))
-            return redirect(redirect_url)
-        else:
-            messages.error(
-                request,
-                u'Something went wrong in your submit. Please, check it.')
+#             redirect_url = reverse('user_detail', args=(request.user.username))
+#             return redirect(redirect_url)
+#         else:
+#             messages.error(
+#                 request,
+#                 u'Something went wrong in your submit. Please, check it.')
 
-    context['form'] = form
-    return render_response(request, 'plugin-submit.html', context)
+#     context['form'] = form
+#     return render_response(request, 'plugin-submit.html', context)

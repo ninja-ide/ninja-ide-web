c69e17ece9f02657911094da8b754189111aa5ff
Pedro Mourelle <pedromagnus@gmail.com>
2014-02-09 20:33:19 -0300

    Production settings.

---
+++ mezza-ninja/settings/prod.py
@@ -0,0 +1,31 @@
+#######################
+# PRODUCTION SETTINGS #
+#######################
+
+from base import *
+
+
+DEBUG = False
+LESS_DEBUG = False
+
+TEST_RUNNER = 'django.test.simple.DjangoTestSuiteRunner'
+
+# all private settings to config production environment
+# should exist in a separated AND NOT VERSIONED file (prod_private).
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": "dev.db",
+#         "USER": "",
+#         "PASSWORD": "",
+#         "HOST": "",
+#         "PORT": "",
+#     }
+# }
+
+try:
+    from prod_private import *
+except:
+    print("Production settings file not found. Ensure you have a "\
+          "'prod_private' file (not versioned)")

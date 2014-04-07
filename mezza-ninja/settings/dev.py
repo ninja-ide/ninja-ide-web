dc52e6f8bafa1cbd3a13a67bf79b29f15173d055
Pedro Mourelle <pedromagnus@gmail.com>
2014-04-07 04:15:31 -0300

    Lessc path

--- mezza-ninja/settings/dev.py
+++ mezza-ninja/settings/dev.py
@@ -10,6 +10,7 @@ from base import *
 
 DEBUG = True
 LESS_DEBUG = True
+LESS_EXECUTABLE = "node_modules/less/bin/lessc"
 COMPRESS_ENABLED = not DEBUG
 
 TEST_RUNNER = 'django.test.simple.DjangoTestSuiteRunner'
